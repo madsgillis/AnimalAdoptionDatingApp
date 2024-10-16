521cdbba6b8b508f8249594c820a46dc41fd3258
derrickjhiggins <derrick.higgins@outlook.com>
2024-10-16 15:40:19 -0500

    init react and flask

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
