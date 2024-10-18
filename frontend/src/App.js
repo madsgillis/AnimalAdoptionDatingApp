ec5f5861a602215f05d090b22625a3946987996a
emmaskillen <emma.skillen28@gmail.com>
2024-10-17 17:49:46 -0700

    created base admin page WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
+import Admin from './components/admin';
 
 function App() {
+
+
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <li><Link to="/">Home</Link></li>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +24,22 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    */
+      <Router>
+        <div className="App">
+            <nav>
+                <ul>
+                    <li><Link to="/admin">Admin</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/admin" element={<Admin />} />
+            </Routes>
+        </div>
+    </Router>
+    /*</div>*/
   );
 }
 
 export default App;
+
