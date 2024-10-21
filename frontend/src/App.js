70d5fd384030955d939eb4be07ae8539b4248431
emmaskillen <emma.skillen28@gmail.com>
2024-10-21 10:19:02 -0700

    removed routing from app.js for now, base page for admin created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
-import Admin from './components/admin';
 
 function App() {
-
-
   return (
-    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <li><Link to="/">Home</Link></li>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -24,22 +18,8 @@ function App() {
           Learn React
         </a>
       </header>
-    */
-      <Router>
-        <div className="App">
-            <nav>
-                <ul>
-                    <li><Link to="/admin">Admin</Link></li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/admin" element={<Admin />} />
-            </Routes>
-        </div>
-    </Router>
-    /*</div>*/
+    </div>
   );
 }
 
 export default App;
-
