1ae58d55b44921a0f2f0da56c4c6178af1518740
derrickjhiggins <derrick.higgins@outlook.com>
2024-10-22 23:24:57 -0500

    revert app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
-import Admin from './components/admin';
-import AdoptionFeed from './components/feed/news-feed';
 
-/* routes: adding separate pages for REACT */
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/feed" element={<AdoptionFeed />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
