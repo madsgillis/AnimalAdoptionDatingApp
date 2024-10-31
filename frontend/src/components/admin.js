46a031c7a346adf3053eff857b9ecb8f07a1404e
Alicia Curtis <aliciahelen22@gmail.com>
2024-10-30 23:31:01 -0700

    connected animal profile database table to admin page

--- frontend/src/components/admin.js
+++ frontend/src/components/admin.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AdminTable from './admin-table';
 import '../App.css';
 
-// while hardcoding in data until using database: use imports for images
-import charlesImage from './images/pexels-charles.jpg';
-import lilyImage from './images/Lily.jpg';
-import FreddyImage from './images/pexels-freddy.jpg';
-import lincolnImage from './images/lincoln.jpg';
-
-const animalData = [
-    { id: 1, name: 'Charles',  photo: charlesImage, species:'Dog', status:'Adopted' },
-    { id: 3, name: 'Freddy',  photo: FreddyImage, species:'Dog', status:'Available' },
-    { id: 2, name: 'Lily',  photo: lilyImage, species:'Cat', status:'On Hold' },
-    { id: 4, name: 'Lincoln',  photo: lincolnImage, species:'Bird', status:'Currently Unavailable' },
-  ];
-
 function Admin() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/admin');
+                console.log(response);
+                if (!response.ok) {
+                    throw new Error('Network did not respond');
+                }
+
+                // Parse response as JSON
+                const result = await response.json();
+                console.log("Fetched data:", result); // Log parsed JSON result
+                setData(result);
+
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (data.length === 0) return <p>No data available.</p>;
+
     return (
         <div>
             <div style={{ marginBottom: '50px' , backgroundColor: '#979feb'}} fluid className="text-black py-3">
@@ -28,7 +41,7 @@ function Admin() {
             </div>
             <div class="container">
             <h2 style={{textAlign: 'left' }}>Current Animal Profiles</h2>
-                <AdminTable data={animalData}/>
+                <AdminTable data={data}/>
             </div>
         </div>
     );
