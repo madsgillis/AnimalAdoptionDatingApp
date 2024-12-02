8dc6fc08236d2a8e7e7364b5321dc56c59cc2077
emmaskillen <emma.skillen28@gmail.com>
2024-12-01 16:18:09 -0800

    fixed issue with module being found

--- frontend/src/components/admin/admin.js
+++ frontend/src/components/admin/admin.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './admin.css'
-import useFetchData from './use-fetch-data';
+import useFetchData from './use-fetch-data.js';
 
 // icons
 import { Plus } from 'react-bootstrap-icons';
